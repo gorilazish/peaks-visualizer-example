730aeeccf3ce76d180b79837dc8e67a46d6606ff
Ignas <ignas.bagdon@gmail.com>
2018-06-15 13:19:53 +0200

    minimal UI changes

--- src/containers/Visualizer/Visualizer.tsx
+++ src/containers/Visualizer/Visualizer.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import _ from 'lodash'
 import Peaks from 'peaks.js'
 import faker from 'faker'
-import { Flex } from 'reflexbox'
+import { Flex, Box } from 'reflexbox'
 import { Button } from 'antd'
 import 'antd/dist/antd.css'
 import './Visualizer.scss'
@@ -19,12 +19,14 @@ interface ITranscript {
 
 interface IState {
   transcripts: ITranscript[]
+  isLoading: boolean
 }
 
 class Visualizer extends React.Component<{}, IState> {
 
   public state: IState = {
-    transcripts: []
+    transcripts: [],
+    isLoading: true,
   }
 
   public componentDidMount() {    
@@ -39,25 +41,32 @@ class Visualizer extends React.Component<{}, IState> {
       if (!transcriptsGenerated) {
         const soundDuration = Math.floor(peaks.player.getDuration())
         const generatedTranscripts = this.generateTranscript(soundDuration)
-        this.setState({ transcripts: generatedTranscripts })
+        this.setState({ transcripts: generatedTranscripts, isLoading: false })
         transcriptsGenerated = true
       }
     })
   }
 
   public render() {
+
     return (
       <div className='App'>
         <div id='peaks-container'/>
         <audio src={mp3_file}/>
-        <Flex justify={'center'} p={2}>
-          <Button onClick={this.play}>Play</Button>
-          <Button onClick={this.pause}>Pause</Button>
+        {!this.state.isLoading ? <Box>
+          <Flex justify={'center'} p={2}>
+            <Button onClick={this.play}>Play</Button>
+            <Button onClick={this.pause}>Pause</Button>
+          </Flex>
+          <Flex column={true} mx={50}>
+            <h2>People names detected in a call</h2>
+            {(this.state.transcripts.length > 0) && this.state.transcripts.map(t => <Button onClick={() => this.handleItemClick(t)} key={t.startTime}>{t.labelText}</Button>)}
+          </Flex>
+        </Box> : 
+        <Flex justify={'center'}>
+          <h2>Waveform is loading...</h2>
         </Flex>
-        <Flex column={true}>
-          <h2>People names detected in a call</h2>
-          {(this.state.transcripts.length > 0) && this.state.transcripts.map(t => <Button onClick={() => this.handleItemClick(t)} key={t.startTime}>{t.labelText}</Button>)}
-        </Flex>
+        }
       </div>     
     )
   }
@@ -77,7 +86,7 @@ class Visualizer extends React.Component<{}, IState> {
   private generateTranscript(audioDuration: number) {
     const getRandomDuration = () => _.random(0, 30, false)
 
-    let nextStart = 0
+    let nextStart = 10
     let segmentDuration = getRandomDuration()
     let gap = getRandomDuration()
     const transcript = []
